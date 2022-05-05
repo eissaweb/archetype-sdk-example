edaaae2eb44ba210f9a0964bf10ebf000d19f69e
Eissaweb <eissaweb@gmail.com>
2022-05-05 16:28:00 +0100

    Published SDK config.

---
+++ archetype.php
@@ -0,0 +1,15 @@
+<?php 
+
+return [
+    
+    /**
+     * The app_id of your Archetype application
+     * 
+     */
+    'app_id' => env('ARCHETYPE_APP_ID'),
+    /**
+     * The secret_key of your Archetype application
+     * 
+     */
+    'secret_key' => env('ARCHETYPE_SECRET_KEY'),
+];
